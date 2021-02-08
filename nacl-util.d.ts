dc7e444c7fbbf53776bf46b81534d8be0e199f02
developer <team-developer@gmail.com>
2021-02-08 17:58:09 +0800

    init utils

---
+++ nacl-util.d.ts
@@ -0,0 +1,9 @@
+declare var util: util;
+export = util;
+
+interface util {
+    decodeUTF8(s: string): Uint8Array;
+    encodeUTF8(arr: Uint8Array): string;
+    encodeBase64(arr: Uint8Array): string;
+    decodeBase64(s: string): Uint8Array;
+}
